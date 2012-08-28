8d1206181aa5df0431c408b51620e729832fe33e
Jenso <jenso1988@gmail.com>
2012-08-28 13:21:27 +0000

    Added requirements

--- pinry/settings/production.py
+++ pinry/settings/production.py
@@ -9,8 +9,10 @@ TEMPLATE_DEBUG = DEBUG
 # TODO: I recommend using psycopg2 w/ postgres but sqlite3 is good enough.
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(SITE_ROOT, 'production.db'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'projecty',                      
+        'USER': 'projecty',
+        'PASSWORD': 'dxobbmei',
     }
 }
 
